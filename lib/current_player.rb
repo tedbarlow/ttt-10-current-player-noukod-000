10c54afedf2335a2da7f65351fd86981e43c7419
Ted Barlow <ted@noukod.com>
2018-09-03 14:42:08 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,7 @@ def turn_count(board)
 end
 
   def current_player(board)
-    if turn_count(board) == 1 || turn_count(board) % 2 == 0
+    if turn_count(board) == 1 || turn_count(board) % 2 == 1
       return "X"
     else
       return "O"
